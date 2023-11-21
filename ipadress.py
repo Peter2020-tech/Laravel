b6b0772222266c02780c534250f2793dac7f0bdb
Peter2020-tech <64385742+Peter2020-tech@users.noreply.github.com>
2023-11-21 09:13:41 +0100

    Add files via upload

---
+++ ipadress.py
@@ -0,0 +1,15 @@
+import nmap
+
+def scan_target_ports(target_ip):
+    nm = nmap.PortScanner()
+    nm.scan(target_ip, arguments='-p 1-65535')
+    open_ports = []
+    for host in nm.all_hosts():
+        for proto in nm[host].all_protocols():
+            for port in nm[host][proto]:
+                open_ports.append(port)
+    return open_ports
+
+target_ip = "104.17.235.32"
+open_ports = scan_target_ports(target_ip)
+print("Open ports on target:", open_ports)
